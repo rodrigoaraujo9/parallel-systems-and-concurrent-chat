aad97daaf38ef135d34f4c54e00a07b2dcc0173f
Eduardo <edumrcunha@gmail.com>
2025-02-18 17:13:26 +0000

    linebyline

--- assign1/src/matrix-multiplication-example.cpp
+++ assign1/src/matrix-multiplication-example.cpp
@@ -1,4 +1,4 @@
-#include "include/papi.h"
+#include <papi.h>
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
@@ -62,7 +62,58 @@ void OnMult(int m_ar, int m_br) {
 }
 
 // add code here for line x line matriz multiplication
-void OnMultLine(int m_ar, int m_br) {}
+void OnMultLine(int m_ar, int m_br) {
+  SYSTEMTIME Time1, Time2;
+
+  char st[100];
+  double temp;
+  int i, j, k;
+
+  double *pha, *phb, *phc, *aux;
+
+  pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+  phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+  phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+  for (i = 0; i < m_ar; i++)
+    for (j = 0; j < m_ar; j++)
+      pha[i * m_ar + j] = (double)1.0;
+
+  for (i = 0; i < m_br; i++)
+    for (j = 0; j < m_br; j++)
+      phb[i * m_br + j] = (double)(i + 1);
+
+  for (i = 0; i < m_ar; i++)
+    for (j = 0; j < m_ar; j++)
+      phc[i * m_ar + j] = (double)0.0;
+
+  Time1 = clock();
+
+  for (i = 0; i < m_ar; i++) {
+    for (j = 0; j < m_br; j++) {
+      temp = pha[i*m_ar+j];
+      for (k = 0; k < m_ar; k++) {
+        phc[i*m_ar+k] += temp * phb[j*m_br+k];
+      }
+    }
+  }
+
+  Time2 = clock();
+  sprintf(st, "Time: %3.3f seconds\n",
+          (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+  cout << st;
+
+  // display 10 elements of the result matrix tto verify correctness
+  cout << "Result matrix: " << endl;
+  for (i = 0; i < 1; i++) {
+    for (j = 0; j < min(10, m_br); j++)
+      cout << phc[j] << " ";
+  }
+  cout << endl;
+
+  free(pha);
+  free(phb);
+  free(phc);
 
 // add code here for block x block matriz multiplication
 void OnMultBlock(int m_ar, int m_br, int bkSize) {}
