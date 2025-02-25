53038720e943737633b40308f254313509764da5
mxriana6 <marianapp64@gmail.com>
2025-02-25 00:24:00 +0000

    block multiplication

--- assign1/src/matrix-multiplication.cpp
+++ assign1/src/matrix-multiplication.cpp
@@ -48,7 +48,7 @@ void OnMult(int m_ar, int m_br) {
           (double)(Time2 - Time1) / CLOCKS_PER_SEC);
   cout << st;
 
-  // display 10 elements of the result matrix tto verify correctness
+  // display 10 elements of the result matrix to verify correctness
   cout << "Result matrix: " << endl;
   for (i = 0; i < 1; i++) {
     for (j = 0; j < min(10, m_br); j++)
@@ -77,7 +77,7 @@ void OnMultLine(int m_ar, int m_br) {
   phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
   phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
 
-
+  // Initialize matrices
   for (i = 0; i < m_ar; i++) {
     for (j = 0; j < m_ar; j++) {
       pha[i * m_ar + j] = double(1.0);
@@ -90,15 +90,22 @@ void OnMultLine(int m_ar, int m_br) {
     }
   }
 
-  Time1 = clock();
-
+  // Initialize result matrix
   for (i = 0; i < m_ar; i++) {
     for (j = 0; j < m_br; j++) {
-      temp = 0;
+      phc[i * m_ar + j] = double(0.0);
+    }
+  }
+
+  Time1 = clock();
+
+  // Line x line multiplication
+  for (i = 0; i < m_ar; i++) {
+    for (j = 0; j < m_br; j++) {
       for (k = 0; k < m_ar; k++) {
-        temp += pha[i * m_ar + k] * phb[k * m_ar + j];
+        // C[i][j] = ∑ A[i][k] x B[k][j]
+        phc[i * m_ar + j] += pha[i * m_ar + k] * phb[k * m_ar + j]; 
       }
-      phc[i * m_ar + j] = temp;
     }
   }
 
@@ -107,7 +114,7 @@ void OnMultLine(int m_ar, int m_br) {
           (double)(Time2 - Time1) / CLOCKS_PER_SEC);
   cout << st;
 
-  // display 10 elements of the result matrix tto verify correctness
+  // display 10 elements of the result matrix to verify correctness
   cout << "Result matrix: " << endl;
   for (i = 0; i < 1; i++) {
     for (j = 0; j < min(10, m_br); j++)
@@ -121,7 +128,82 @@ void OnMultLine(int m_ar, int m_br) {
 }
 
 // add code here for block x block matriz multiplication
-void OnMultBlock(int m_ar, int m_br, int bkSize) {}
+void OnMultBlock(int m_ar, int m_br, int bkSize) {
+
+  SYSTEMTIME Time1, Time2;
+
+  char st[100];
+  double temp;
+  int i, j, k;
+  int i2, j2, k2;
+
+  double *pha, *phb, *phc;
+
+  pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+  phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+  phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+  // Initialize matrices
+  for (i = 0; i < m_ar; i++) {
+    for (j = 0; j < m_ar; j++) {
+      pha[i * m_ar + j] = double(1.0);
+    }
+  }
+
+  for (i = 0; i < m_br; i++) {
+    for (j = 0; j < m_br; j++) {
+      phb[i * m_br + j] = double(i+1);
+    }
+  }
+
+  // Initialize result matrix
+  for (i = 0; i < m_ar; i++) {
+    for (j = 0; j < m_br; j++) {
+      phc[i * m_ar + j] = double(0.0);
+    }
+  }
+
+  Time1 = clock();
+
+  // Iterate over the sub-blocks of the matrices
+  for (i2 = 0; i2 < m_ar; i2 += bkSize) { // Rows of A
+    for (j2 = 0; j2 < m_br; j2 += bkSize) { // Columns of B
+      for (k2 = 0; k2 < m_ar; k2 += bkSize) {
+
+        // Iterate within each block
+        for (i = i2; i < min(i2 + bkSize, m_ar); i++) { // Rows within block
+          for (j = j2; j < min(j2 + bkSize, m_br); j++) { // Columns within block
+            temp = phc[i * m_ar + j]; // Store accumulated value 
+
+            for (k = k2; k < min(k2 + bkSize, m_ar); k++) {
+              temp += pha[i * m_ar + k] * phb[k * m_br + j]; // Partial sum for corresponding block
+            }
+            phc[i * m_ar + j] = temp; // Store result in result matrix (C)
+          }
+        }
+      }
+    }
+  }
+
+
+  Time2 = clock();
+  sprintf(st, "Time: %3.3f seconds\n",
+          (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+  cout << st;
+
+  // display 10 elements of the result matrix to verify correctness
+  cout << "Result matrix: " << endl;
+  for (i = 0; i < 1; i++) {
+    for (j = 0; j < min(10, m_br); j++)
+      cout << phc[j] << " ";
+  }
+  cout << endl;
+
+
+  free(pha);
+  free(phb);
+  free(phc);
+}
 
 void handle_error(int retval) {
   printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
