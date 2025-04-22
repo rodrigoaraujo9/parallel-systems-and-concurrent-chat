c77b0198397ef092b4e8a753430af1b2cf944ded
mxriana6 <marianapp64@gmail.com>
2025-04-22 10:10:47 +0100

    Started ChatClient

---
+++ assign2/src/ChatClient.java
@@ -0,0 +1,24 @@
+
+
+public class ChatClient {
+
+    private final String name;
+    private final int id;
+    private final String password;
+    private final String token;
+
+    ChatClient(String name, int id, String password, String token) {
+        this.name = name;
+        this.id = id;
+        this.password = password;
+        this.token = token;
+    }
+
+    public void connect() {
+
+    }
+
+    private void authenticate() {
+
+    }
+}
